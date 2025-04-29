e0b1ad8b6af041bdd2bfdb79a3b28ac8023860c7
nahomi2004 <nahomicabrera2004@gmail.com>
2025-04-29 18:45:15 -0500

    Proyecto Vite con jQuery y Datatables El Richito y La Nanami

--- src/main.js
+++ src/main.js
@@ -1,26 +1,38 @@
-import $ from 'jquery';
+import jQuery from 'jquery';
+import dt from '../node_modules/datatables.net-dt'; // importa el estilo también
+import '../node_modules/datatables.net-dt/css/dataTables.dataTables.css';
 
-$(document).ready(function () {
+
+
+jQuery(document).ready(function () {
   
-  const mi_tabla = $('#tabla-ejemplo').DataTable();
+  if (typeof jQuery != 'undefined') {  
+    // jQuery is loaded => print the version
+    alert(jQuery.fn.jquery);
+  }
+
+  const mi_tabla = jQuery('#tabla-ejemplo').DataTable();
 
   var url = "https://pkgstore.datahub.io/core/continent-codes/continent-codes_json/data/60d6baef1250bc2b01fd0148dccca518/continent-codes_json.json";
 
-  $("#obtenerInformacion").click(function() {
-    $.ajax({
+  jQuery("#obtenerInformacion").click(function() {
+    jQuery.ajax({
       dataType: 'json',
       url: url
     }).then(function(data){
+      
       data.forEach(function(item) {
         mi_tabla.row.add([
           item.Code,
           item.Name
         ]).draw(false);
       });
+
     });
   });
 
-  $("#limpiarInformacion").click(function() {
+  jQuery("#limpiarInformacion").click(function() {
     mi_tabla.clear().draw();
-  })
+  });
+
 });
